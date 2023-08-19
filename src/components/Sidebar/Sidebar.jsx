f85d7f787a9b0b3805d900a381acea0925e3f290
syedjunaid7 <99808491+syedjunaid7@users.noreply.github.com>
2023-08-19 17:52:09 +0530

    initial commit

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import "./Sidebar.scss";
 import logo from "../../imgs/logo.png";
+import { UilEstate } from "@iconscout/react-unicons";
+// import { SidebarData } from "../Data/Data";
+// import { UilBars } from "@iconscout/react-unicons";
 const Sidebar = () => {
   return (
     <div className="Sidebar">
-      {/* {logo} */}
+
       <div className="logo">
         <img src={logo} alt="logo" />
         <span>
           Little<span>Millenium</span>Convent
         </span>
+      </div>
+      <div className="menu">
+        <div className="menuItem">
+          <div>
+          <UilEstate />
+          </div>
+          <span>DashBoard</span>
+        </div>
       </div>
     </div>
   );
