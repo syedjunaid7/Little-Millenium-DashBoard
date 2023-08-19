adf51cf3da09d255c200b427f9654c8f4c239536
syedjunaid7 <99808491+syedjunaid7@users.noreply.github.com>
2023-08-19 18:02:49 +0530

    initial commit

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.scss";
 import logo from "../../imgs/logo.png";
-
-// import { SidebarData } from "../Data/Data";
-// import { UilBars } from "@iconscout/react-unicons";
+import { SidebarData } from "../../Data/Data";
+import { UilSignOutAlt } from "@iconscout/react-unicons";
+import { UilBars } from "@iconscout/react-unicons";
 const Sidebar = () => {
+  const [selected, setSelected] = useState(0);
   return (
     <div className="Sidebar">
-
       <div className="logo">
         <img src={logo} alt="logo" />
         <span>
           Little<span>Millenium</span>Convent
         </span>
       </div>
+
       <div className="menu">
+        {SidebarData.map((item, index) => {
+          return (
+            <div
+              className={selected === index ? "menuItem active" : "menuItem"}
+              key={index}
+              onClick={() => setSelected(index)}
+            >
+              <item.icon />
+              <span>{item.heading}</span>
+            </div>
+          );
+        })}
         <div className="menuItem">
-          <div>
-          <UilEstate />
-          </div>
-          <span>DashBoard</span>
+          <UilSignOutAlt />
         </div>
       </div>
     </div>
