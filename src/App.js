6d2dbe985b3811a7dc270cb3d9700e3da5789a31
syedjunaid7 <99808491+syedjunaid7@users.noreply.github.com>
2023-08-20 18:46:14 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css'
+import MainDash from './components/MainDash/MainDash';
 import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
@@ -6,8 +7,9 @@ function App() {
     <div className="App">
       <div className="AppGlass">
       <Sidebar/>
+      <MainDash/>
         {/* 
-        <MainDash/>
+        
         <RightSide/> */}
       </div>
     </div>
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import "./Cards.scss"
+export default function Cards() {
+  return (
+    
+  )
+}
