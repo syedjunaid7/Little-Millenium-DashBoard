0421cc4abafc4588c5ac05a06feb24c5a57a3a78
syedjunaid7 <99808491+syedjunaid7@users.noreply.github.com>
2023-08-20 19:14:35 +0530

    c

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,21 +4,21 @@ import { cardsData } from '../../Data/Data'
 import Card from '../Card/Card';
 export default function Cards() {
   return (
-<div className="Cards">
-      {cardsData.map((card, id) => {
-        return (
-          <div className="parentContainer" key={id}>
-            <Card
-              title={card.title}
-              color={card.color}
-              barValue={card.barValue}
-              value={card.value}
-              png={card.png}
-              series={card.series}
-            />
-          </div>
-        );
-      })}
-    </div>
+    <div className="Cards">
+    {cardsData.map((card, id) => {
+      return (
+        <div className="parentContainer" key={id}>
+          <Card
+            title={card.title}
+            color={card.color}
+            barValue={card.barValue}
+            value={card.value}
+            png={card.png}
+            series={card.series}
+          />
+        </div>
+      );
+    })}
+  </div>
   )
 }
