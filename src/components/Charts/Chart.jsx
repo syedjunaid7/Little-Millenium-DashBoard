b75c3260c36980f31620a2cf18efc02283bb4215
syedjunaid7 <99808491+syedjunaid7@users.noreply.github.com>
2023-08-27 20:14:45 +0530

    c

--- src/components/Charts/Chart.jsx
+++ src/components/Charts/Chart.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ApexCharts from "react-apexcharts"; // Make sure to import from the correct package
-import "./Chart.scss"
+import "./Chart.scss";
 export default function Chart() {
   const [state, setState] = useState({
     options: {
@@ -33,6 +33,36 @@ export default function Chart() {
     },
   });
 
+const [state2, setState2] = useState({
+  options: {
+    chart: {
+      type: "line",
+    },
+    series: [
+      {
+        name: "Boys",
+        data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+      },
+      {
+        name: "Girls",
+        data: [10, 20, 55, 40, 39, 40, 80, 41, 135],
+      },
+    ],
+    xaxis: {
+      categories: [
+        "Jan",
+        "Feb",
+        "Feb",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+      ],
+    },
+  },
+});
   useEffect(() => {
     var chart = new ApexCharts(document.querySelector("#chart"), state.options);
     chart.render();
@@ -44,7 +74,13 @@ export default function Chart() {
         options={state.options}
         series={state.options.series}
         type="line"
-        width="500"
+        width="450"
+      />
+      <ApexCharts
+        options={state2.options}
+        series={state2.options.series}
+        type="bar"
+        width="450"
       />
     </div>
   );
